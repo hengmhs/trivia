d77db0bf7a418203033d18bb25d2b92337ba7bc7
hengmhs <hengmhs@gmail.com>
2023-04-29 13:03:24 +0800

    Move Quiz Component from Home to GameLobby

--- src/Components/GameLobby.js
+++ src/Components/GameLobby.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../App.css";
 import Quiz from "./Quiz";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { database, auth } from "../firebase";
 import {
@@ -12,6 +12,8 @@ import {
   runTransaction,
 } from "firebase/database";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import he from "he";
 
 // Objective: Users can join room via the React Router Link, and can increment/decrement counters
 
@@ -23,6 +25,30 @@ const GameLobby = () => {
   const [connectedPlayers, setConnectedPlayers] = useState({});
   const [scores, setScores] = useState({ placeholder: 0 });
   const [userUID, setUserUID] = useState("");
+  const [quizText, setQuizText] = useState("Waiting for Question");
+  const [currentQuestionData, setCurrentQuestionData] = useState({
+    category: "placeholder",
+    correct_answer: "placeholder",
+    difficulty: "placeholder",
+    incorrect_answers: "placeholder",
+    question: "placeholder",
+    type: "placeholder",
+  });
+  const [currentOptions, setCurrentOptions] = useState(["A", "B", "C", "D"]);
+  const [currentAnswer, setCurrentAnswer] = useState(null);
+  const [score, setScore] = useState(0);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [questionList, setQuestionList] = useState([
+    {
+      category: "placeholder",
+      correct_answer: "placeholder",
+      difficulty: "placeholder",
+      incorrect_answers: ["placeholder", "placeholder", "placeholder"],
+      question: "placeholder",
+      type: "placeholder",
+    },
+  ]);
+  const firstRender = useRef(true);
 
   // get the room key from the URL, params refers to path="/room/:roomKey"
   const { roomKey } = useParams();
@@ -109,6 +135,93 @@ const GameLobby = () => {
     });
   };
 
+  useEffect(() => {
+    loadQuestion(0);
+  }, [questionList]);
+
+  useEffect(() => {
+    // do not trigger this on the first render
+    if (firstRender.current) {
+      firstRender.current = false;
+    } else {
+      console.log("Question Index: ", currentQuestionIndex);
+      if (currentQuestionIndex < questionList.length) {
+        loadQuestion(currentQuestionIndex);
+      } else {
+        console.log("Game Over, Restarting");
+      }
+    }
+  }, [currentQuestionIndex]);
+
+  useEffect(() => {
+    // use the he module to decode the HTML response from the API
+    setCurrentAnswer(he.decode(currentQuestionData.correct_answer));
+    const options = [
+      ...currentQuestionData.incorrect_answers,
+      currentQuestionData.correct_answer,
+    ];
+    // Durstenfeld Shuffle from Stack Overflow: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    const shuffleArray = (array) => {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+    };
+    setCurrentOptions(
+      shuffleArray(
+        options.map((option) => {
+          return he.decode(option);
+        })
+      )
+    );
+  }, [currentQuestionData]);
+
+  const loadQuestion = (questionListIndex) => {
+    setCurrentQuestionData(questionList[questionListIndex]);
+  };
+
+  const getQuestions = () => {
+    axios
+      .get("https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple")
+      .then((res) => {
+        setQuestionList(res.data.results);
+        setCurrentQuestionIndex(0);
+      });
+    /*
+      res.data.results -> Array
+      res.data.results[0] = {
+        category: str,
+        correct_answer: str,
+        difficulty: str, (Easy, Medium, Hard)
+        incorrect_answers: Array,
+        question: str,
+        type: str
+      }
+      */
+  };
+
+  const submitAnswer = (currentOptionsPosition) => {
+    const userAnswer = currentOptions[currentOptionsPosition];
+    console.log(userAnswer);
+    if (currentQuestionIndex === questionList.length) {
+      setQuizText(`Game Over.`);
+      return;
+    }
+    if (currentAnswer === userAnswer) {
+      setQuizText(`Correct! ${currentAnswer} was the answer.`);
+      increaseScore();
+      setCurrentQuestionIndex((currentQuestionIndex) => {
+        return currentQuestionIndex + 1;
+      });
+    } else {
+      setQuizText(`Wrong, the correct answer was: ${currentAnswer}`);
+      setCurrentQuestionIndex((currentQuestionIndex) => {
+        return currentQuestionIndex + 1;
+      });
+    }
+  };
+
   return (
     <div className="App">
       <div>Name: {displayName}</div>
@@ -133,6 +246,17 @@ const GameLobby = () => {
             );
           })}
         </div>
+        {true && (
+          <Quiz
+            displayName={displayName}
+            score={score}
+            currentQuestionData={currentQuestionData}
+            getQuestions={getQuestions}
+            submitAnswer={submitAnswer}
+            currentOptions={currentOptions}
+            quizText={quizText}
+          />
+        )}
       </div>
       <button onClick={increaseScore}>Increase My Score</button>
     </div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,45 +1,17 @@
 import React from "react";
 import "../App.css";
-import Quiz from "./Quiz";
 import AuthForm from "./AuthForm";
 import LoginForm from "./LoginForm";
-import { useState, useEffect, useRef } from "react";
-import axios from "axios";
-import he from "he";
+import { useState, useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { database, auth } from "../firebase";
-import { set, ref, push, onChildAdded } from "firebase/database";
+import { set, ref, push } from "firebase/database";
 import GameFeed from "./GameFeed.js";
 
 const DB_ROOM_KEY = "rooms";
-const DB_SCORE_KEY = "scores";
 
 const Home = () => {
   const [displayName, setDisplayName] = useState("None");
-  const [quizText, setQuizText] = useState("Waiting for Question");
-  const [currentQuestionData, setCurrentQuestionData] = useState({
-    category: "placeholder",
-    correct_answer: "placeholder",
-    difficulty: "placeholder",
-    incorrect_answers: "placeholder",
-    question: "placeholder",
-    type: "placeholder",
-  });
-  const [currentOptions, setCurrentOptions] = useState(["A", "B", "C", "D"]);
-  const [currentAnswer, setCurrentAnswer] = useState(null);
-  const [score, setScore] = useState(0);
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [questionList, setQuestionList] = useState([
-    {
-      category: "placeholder",
-      correct_answer: "placeholder",
-      difficulty: "placeholder",
-      incorrect_answers: ["placeholder", "placeholder", "placeholder"],
-      question: "placeholder",
-      type: "placeholder",
-    },
-  ]);
-  const firstRender = useRef(true);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -50,98 +22,8 @@ const Home = () => {
         // User is signed out
       }
     });
-    const roomRef = ref(database, DB_ROOM_KEY);
-    onChildAdded(roomRef, (data) => {
-      console.log(data.val().roomName);
-    });
   }, []);
 
-  useEffect(() => {
-    loadQuestion(0);
-  }, [questionList]);
-
-  useEffect(() => {
-    // do not trigger this on the first render
-    if (firstRender.current) {
-      firstRender.current = false;
-    } else {
-      console.log("Question Index: ", currentQuestionIndex);
-      if (currentQuestionIndex < questionList.length) {
-        loadQuestion(currentQuestionIndex);
-      } else {
-        console.log("Game Over, Restarting");
-      }
-    }
-  }, [currentQuestionIndex]);
-
-  useEffect(() => {
-    // use the he module to decode the HTML response from the API
-    setCurrentAnswer(he.decode(currentQuestionData.correct_answer));
-    const options = [
-      ...currentQuestionData.incorrect_answers,
-      currentQuestionData.correct_answer,
-    ];
-    // Durstenfeld Shuffle from Stack Overflow: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-    const shuffleArray = (array) => {
-      for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-      }
-      return array;
-    };
-    setCurrentOptions(
-      shuffleArray(
-        options.map((option) => {
-          return he.decode(option);
-        })
-      )
-    );
-  }, [currentQuestionData]);
-
-  const loadQuestion = (questionListIndex) => {
-    setCurrentQuestionData(questionList[questionListIndex]);
-  };
-  const getQuestions = () => {
-    axios
-      .get("https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple")
-      .then((res) => {
-        setQuestionList(res.data.results);
-        setCurrentQuestionIndex(0);
-      });
-    /*
-      res.data.results -> Array
-      res.data.results[0] = {
-        category: str,
-        correct_answer: str,
-        difficulty: str, (Easy, Medium, Hard)
-        incorrect_answers: Array,
-        question: str,
-        type: str
-      }
-      */
-  };
-  const submitAnswer = (currentOptionsPosition) => {
-    const userAnswer = currentOptions[currentOptionsPosition];
-    console.log(userAnswer);
-    if (currentQuestionIndex === questionList.length) {
-      console.log("No more questions");
-      return;
-    }
-    if (currentAnswer === userAnswer) {
-      setQuizText("Correct!");
-      setScore((score) => {
-        return score + 1;
-      });
-      setCurrentQuestionIndex((currentQuestionIndex) => {
-        return currentQuestionIndex + 1;
-      });
-    } else {
-      setQuizText(`Wrong, the correct answer was: ${currentAnswer}`);
-      setCurrentQuestionIndex((currentQuestionIndex) => {
-        return currentQuestionIndex + 1;
-      });
-    }
-  };
   const createRoom = (e) => {
     e.preventDefault();
     const roomName = e.target[0].value;
@@ -159,18 +41,6 @@ const Home = () => {
   return (
     <div className="App">
       <div>Name: {displayName}</div>
-      {true && (
-        <Quiz
-          displayName={displayName}
-          score={score}
-          currentQuestionData={currentQuestionData}
-          getQuestions={getQuestions}
-          submitAnswer={submitAnswer}
-          currentOptions={currentOptions}
-          quizText={quizText}
-        />
-      )}
-
       <div>
         {true && <AuthForm setDisplayName={setDisplayName} />}
         <LoginForm setDisplayName={setDisplayName} />
