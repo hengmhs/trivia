377c534e30c6cefc8408d0de0c001e4c6b7a958c
prettykaur <pkaur.dev@gmail.com>
2023-05-02 18:34:10 +0800

    Wrap routes with AuthProvider

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import Home from "./Components/Home";
 import GameLobby from "./Components/GameLobby";
 import InvalidRoom from "./Components/InvalidRoom";
 import { BrowserRouter, Routes, Link, Route } from "react-router-dom";
+import { AuthProvider } from "./AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/invalid" element={<InvalidRoom />} />
-      <Route path="/room/:roomKey" element={<GameLobby />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/invalid" element={<InvalidRoom />} />
+        <Route path="/room/:roomKey" element={<GameLobby />} />
+      </Routes>
+    </AuthProvider>
   </BrowserRouter>
 );
