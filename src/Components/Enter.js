a252a180dfaf2b5a85b41b7828adc44447fc4243
prettykaur <pkaur.dev@gmail.com>
2023-05-04 15:28:37 +0800

    Add login error message to Enter page

--- src/Components/Enter.js
+++ src/Components/Enter.js
@@ -1,15 +1,20 @@
 // First page users see
 // Navigate to Login/Register page via buttons
 
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../AuthContext";
+import LoginError from "./LoginError";
 
 function Enter() {
+  const { user } = useContext(AuthContext);
+
   return (
     <div className="App auth-ctn">
       <div className="auth">
         <div>Logo goes here</div>
         <h1>Trivia Game</h1>
+        {!user ? <LoginError /> : null}
         <div className="enter-btns">
           <Link to="/login">
             <button className="auth-btn">LOGIN</button>
