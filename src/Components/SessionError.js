fcf99dab9d819131a2483923e36996672eda61ba
prettykaur <pkaur.dev@gmail.com>
2023-05-04 13:25:52 +0800

    Add session error page

---
+++ src/Components/SessionError.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SessionError() {
+  return (
+    <p>
+      It looks like you are already logged in. Please close this window and
+      return to your current session.
+    </p>
+  );
+}
+
+export default SessionError;
