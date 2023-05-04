6e0d0ad48cd1d53ff578fb9a5d21a2052a258858
hengmhs <hengmhs@gmail.com>
2023-05-04 14:09:19 +0800

    Add star design to Enter component

--- src/Components/Enter.js
+++ src/Components/Enter.js
@@ -6,17 +6,22 @@ import { Link } from "react-router-dom";
 
 function Enter() {
   return (
-    <div className="App auth-ctn">
-      <div className="auth">
-        <div>Logo goes here</div>
-        <h1>Trivia Game</h1>
-        <div className="enter-btns">
-          <Link to="/login">
-            <button className="auth-btn">LOGIN</button>
-          </Link>
-          <Link to="/register">
-            <button className="auth-btn">REGISTER</button>
-          </Link>
+    <div>
+      <div className="stars"></div>
+      <div className="stars2"></div>
+      <div className="stars3"></div>
+      <div className="App auth-ctn">
+        <div className="auth">
+          <div>Logo goes here</div>
+          <h1>Trivia Game</h1>
+          <div className="enter-btns">
+            <Link to="/login">
+              <button className="auth-btn">LOGIN</button>
+            </Link>
+            <Link to="/register">
+              <button className="auth-btn">REGISTER</button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
