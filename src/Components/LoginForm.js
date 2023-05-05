6be00faa8afc25ae15fb1f582eaa90d9a07e1709
hengmhs <hengmhs@gmail.com>
2023-05-05 17:40:00 +0800

    Add stars background to LoginForm component

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -8,7 +8,6 @@ import { useAuth } from "../AuthContext";
 import { useNavigate } from "react-router-dom";
 import { TextField } from "@mui/material";
 import Button from "@mui/material/Button";
-import NASA from "../images/NASA.jpg";
 
 function LoginForm() {
   const [email, setEmail] = useState("");
@@ -40,46 +39,51 @@ function LoginForm() {
   };
 
   return (
-    <div className="App auth-ctn">
-      <div className="login-card">
-        <div className="login-card-header">
-          <h1 className="login-card-header-text">Welcome, Explorer!</h1>
-        </div>
-        <div className="login-card-content">
-          {" "}
-          <form onSubmit={handleLogin} className="login-form">
-            <TextField
-              color="primary"
-              label="Email"
-              variant="outlined"
-              className="auth-input"
-              type="email"
-              placeholder="Enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-            <TextField
-              className="auth-input"
-              type="password"
-              label="Password"
-              placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            <Button type="submit" variant="contained" className="login-btn">
-              Login
-            </Button>
-          </form>
-          {error && <p className="error-msg">Error: {error}</p>}
-          <div className="login-text">
-            Don't have an account?{" "}
-            <span>
-              <a className="auth-link" href="/register">
-                Register Now
-              </a>
-            </span>
+    <div className="background">
+      <div className="stars"></div>
+      <div className="stars2"></div>
+      <div className="stars3"></div>
+      <div className="App auth-ctn">
+        <div className="login-card">
+          <div className="login-card-header">
+            <h1 className="login-card-header-text">Welcome, Explorer!</h1>
+          </div>
+          <div className="login-card-content">
+            {" "}
+            <form onSubmit={handleLogin} className="login-form">
+              <TextField
+                color="primary"
+                label="Email"
+                variant="outlined"
+                className="auth-input"
+                type="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <TextField
+                className="auth-input"
+                type="password"
+                label="Password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <Button type="submit" variant="contained" className="login-btn">
+                Login
+              </Button>
+            </form>
+            {error && <p className="error-msg">Error: {error}</p>}
+            <div className="login-text">
+              Don't have an account?{" "}
+              <span>
+                <a className="auth-link" href="/register">
+                  Register Now
+                </a>
+              </span>
+            </div>
           </div>
         </div>
       </div>
