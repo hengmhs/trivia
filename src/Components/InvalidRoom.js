73d076cad00b56f711930063f0b3f40ff3dd13fb
hengmhs <hengmhs@gmail.com>
2023-05-08 13:46:32 +0800

    Added styling and return to Home button for InvalidRoom

--- src/Components/InvalidRoom.js
+++ src/Components/InvalidRoom.js
@@ -1,11 +1,24 @@
 import React from "react";
 import "../App.css";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 const InvalidRoom = () => {
   return (
     <div className="App">
-      <h3>Invalid Room</h3>
-      <div>Sorry, that room doesn't exist.</div>
+      <div className="error-container">
+        <h3>Invalid Room</h3>
+        <div>Sorry, that room doesn't exist.</div>
+        <div>
+          You may have entered the wrong room code or the host may have deleted
+          the room.
+        </div>
+        <div>
+          <Link to="/home">
+            <Button variant="contained">Return Home</Button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
