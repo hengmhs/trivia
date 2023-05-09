e40ad30bd11ca09d8011674f2e29805ed8c2f80a
hengmhs <hengmhs@gmail.com>
2023-05-10 04:26:48 +0800

    Added Invalid Room Alien

--- src/Components/InvalidRoom.js
+++ src/Components/InvalidRoom.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
+import InvalidAlien from "../images/invalid_alien.png";
 
 const InvalidRoom = () => {
   useEffect(() => {
@@ -12,6 +13,10 @@ const InvalidRoom = () => {
     <div className="App">
       <div className="error-container">
         <h3>Invalid Room</h3>
+        <div>
+          <img src={InvalidAlien} alt="Alien" className="invalid-alien" />
+        </div>
+
         <div>Sorry, that room doesn't exist.</div>
         <div>
           You may have entered the wrong room code or the host may have deleted
