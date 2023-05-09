8437eb372c01cf85c5f7e9ed390e981f53abbba7
hengmhs <hengmhs@gmail.com>
2023-05-09 13:57:02 +0800

    Players in GameFeed display update properly

--- src/Components/GameFeed.js
+++ src/Components/GameFeed.js
@@ -1,29 +1,48 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { database } from "../firebase";
-import { ref, onChildAdded } from "firebase/database";
+import { ref, onChildAdded, onValue } from "firebase/database";
 import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
 
 const GameFeed = (props) => {
   const navigate = useNavigate();
   const [openGameList, setOpenGameList] = useState([]);
+  const roomRef = ref(database, props.DB_ROOM_KEY);
   useEffect(() => {
-    const roomRef = ref(database, props.DB_ROOM_KEY);
-    onChildAdded(roomRef, (data) => {
-      setOpenGameList((openGameList) => [
-        ...openGameList,
-        {
-          key: data.key,
-          roomName: data.val().roomName,
-          gameStarted: false,
-          hostUID: data.val().hostUID,
-          hostDisplayName: data.val().hostDisplayName,
-          playerList: data.val().playerList,
-        },
-      ]);
+    onValue(roomRef, (data) => {
+      if (data.val() !== null) {
+        const roomList = [];
+        Object.entries(data.val()).map((roomData) => {
+          console.log(roomData);
+          roomList.push({
+            key: roomData[0],
+            roomName: roomData[1].roomName,
+            gameStarted: roomData[1].gameStarted,
+            hostUID: roomData[1].hostUID,
+            hostDisplayName: roomData[1].hostDisplayName,
+            playerList: roomData[1].playerList,
+          });
+        });
+        setOpenGameList(roomList);
+      }
     });
   }, []);
+
+  /*
+setOpenGameList((openGameList) => [
+          ...openGameList,
+          {
+            key: data.key,
+            roomName: data.val().roomName,
+            gameStarted: false,
+            hostUID: data.val().hostUID,
+            hostDisplayName: data.val().hostDisplayName,
+            playerList: data.val().playerList,
+          },
+        ]);
+  */
+
   let roomDisplay = openGameList.map((room) => {
     return (
       <div className="room-card" key={room.key}>
